7c68dc385a7b7f80fb9bcb33400cd72401580d85
Andre Stackhouse <CaptainStack@outlook.com>
2020-02-08 16:35:18 -0800

    Add basic scene management

---
+++ Assets/SceneController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButton("Cancel"))
+        {
+            QuitGame();
+        }
+        if (Input.GetButton("Submit"))
+        {
+            SceneManager.LoadScene("Game");
+        }
+        if (Input.GetButton("Jump"))
+        {
+            SceneManager.LoadScene("GameOver");
+        }
+    }
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
