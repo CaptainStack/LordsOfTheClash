1390457ac5097c83ef3d92966be2dcc2c6131644
FakeBrett <brettloland@gmail.com>
2020-02-08 17:07:01 -0800

    Create Game Manager
    Added scripts for a game manager "Player"; one script for Hero; and one for Card.

---
+++ Assets/Scripts/Hero.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hero
+{
+    public int hp;
+    public int heroDeckSize;
+    //public int heroHandSize;
+
+    
+
+    void Start()
+    {
+        Card[] allHeroCards = new Card[heroDeckSize];
+        //Card[] heroHand = new Card[heroHandSize]; 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+    }
+}
