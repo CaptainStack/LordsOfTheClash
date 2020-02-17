b7db0afdb0facfe16939ebd3b004ca6e7a27002a
Christopher Proteau <christopherproteau@gmail.com>
2020-02-17 09:50:59 -0800

    comments

--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -4,17 +4,25 @@ using UnityEngine;
 
 public class Spawner : MonoBehaviour
 {
-    public float respawnFrequency;
+    // How long between respawns
+    public float respawnCooldown;
+
+    // Type of unit to spawn
     public Unit unitToSpawn;
+
+    // Faction of spawned units
     public Faction faction;
+
+    // Offset from spawner's position to place spawned unit
     public Vector3 spawnPositionOffset = new Vector3(0f, 0.1f);
 
+    // Time remaining until next spawn
     private float timeUntilNextSpawn;
 
     // Start is called before the first frame update
     void Start()
     {
-        timeUntilNextSpawn = respawnFrequency;
+        timeUntilNextSpawn = respawnCooldown;
     }
 
     // Update is called once per frame
@@ -24,7 +32,7 @@ public class Spawner : MonoBehaviour
 
         if (timeUntilNextSpawn <= 0f)
         {
-            timeUntilNextSpawn = respawnFrequency;
+            timeUntilNextSpawn = respawnCooldown;
 
             Vector3 spawnPosition = this.transform.position + spawnPositionOffset;
             Unit newUnit = Instantiate(unitToSpawn, spawnPosition, Quaternion.identity);
