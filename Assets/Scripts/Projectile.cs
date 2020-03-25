fbc0b26f006add5efa097ff850e90a80bc4f910d
Christopher Proteau <christopherproteau@gmail.com>
2020-03-25 13:11:41 -0700

    less frequent unit movement updates, AoEs use OverlapPointAll when radius == 0, and removed rigidbody2d from projectiles (movement is controlled manually through transform now)

--- Assets/Scripts/Projectile.cs
+++ Assets/Scripts/Projectile.cs
@@ -15,42 +15,39 @@ public class Projectile : MonoBehaviour
     // Position projectile is targetting
     public Vector3 target;
 
-    public Rigidbody2D rigidbody2d;
+    //public Rigidbody2D rigidbody2d;
     public SpriteRenderer spriteRenderer;
 
     private float detonationTimer = 0f;
     
     void Start()
     {
-        // Add RigidBody2D
-        if (!rigidbody2d)
-        {
-            rigidbody2d = this.gameObject.AddComponent<Rigidbody2D>();
-            rigidbody2d.drag = 0.0f;
-            rigidbody2d.gravityScale = 0.0f;
-            rigidbody2d.freezeRotation = true;
-            rigidbody2d.isKinematic = true;
-        }
-
         // Add Sprite
         if (!spriteRenderer)
             spriteRenderer = this.gameObject.AddComponent<SpriteRenderer>();
 
-        MoveToTarget();
+        ComputeDetonationTimer();
+    }
+
+    // Set detonation timer for when the projectile will arrive
+    void ComputeDetonationTimer()
+    {
+        Vector3 movementVector = target - this.transform.position; 
+        detonationTimer = Time.time + movementVector.magnitude / speed;
     }
 
     void Update()
     {
-        // Explode if projectile has reached target
-        if (Time.time > detonationTimer)
-        {
-            Explode();
-        }
     }
 
     // FixedUpdate runs synchronized with Unity physics cycle
     void FixedUpdate()
     {
+        // Explode if projectile has reached target
+        if (Time.time > detonationTimer)
+            Explode();
+        else
+            MoveToTarget();
     }
 
     void Explode()
@@ -67,9 +64,6 @@ public class Projectile : MonoBehaviour
     {
         Vector3 movementVector = target - this.transform.position; 
         Vector3 direction = (movementVector).normalized;
-        rigidbody2d.velocity = direction * speed;
-
-        // Set detonation timer for when the projectile will arrive
-        detonationTimer = Time.time + movementVector.magnitude / speed;
+        this.transform.position += direction * speed * Time.deltaTime;
     }
 }
