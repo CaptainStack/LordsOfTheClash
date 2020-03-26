1a7df2ceafe00992c0653c2366e9d569337ff6ad
Christopher Proteau <christopherproteau@gmail.com>
2020-03-26 15:30:04 -0700

    Add Flying layer to fix flying unit collision (at a small performance cost when acquiring targets, since faction layer can't be used)

--- Assets/Scripts/FlyingUnitEffect.cs
+++ Assets/Scripts/FlyingUnitEffect.cs
@@ -12,14 +12,16 @@ public class FlyingUnitEffect : UnitEffect
         if (unit.transform.position.z < 0 || unit.transform.position.z > 0)
             Destroy(this);
 
-        // Apply flying to target by setting its z-depth to -1
+        // Apply flying to target by setting its z-depth to -1 and moving it to the "Flying" layer
         unit.transform.position = new Vector3(unit.transform.position.x, unit.transform.position.y, -1f);
+        unit.gameObject.layer = LayerMask.NameToLayer("Flying");
     }
 
     // What to do at the end of the effect
     protected override void OnEffectEnd()
     {
-        // Remove flying from target by setting its z-depth to 0
+        // Remove flying from target by setting its z-depth to 0 and resetting its faction layer
         unit.transform.position = new Vector3(unit.transform.position.x, unit.transform.position.y, 0f);
+        unit.InitializeUnitFaction();
     }
 }
