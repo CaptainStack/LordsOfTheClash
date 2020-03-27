8498ebf3ed92a6dafaff7fb59137a992de95005b
Andre Stackhouse <CaptainStack@outlook.com>
2020-03-27 02:56:20 -0700

    Create audio manager class

---
+++ Assets/Scripts/AudioManager.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine.Audio;
+using System;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    public static AudioManager instance;
+    public string sceneMusic;
+    [SerializeField]
+    public Sound[] sounds;
+
+    void Awake()
+    {
+        if (instance != null)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+
+        foreach (Sound sound in sounds)
+        {
+            sound.source = gameObject.AddComponent<AudioSource>();
+            sound.source.clip = sound.clip;
+            sound.source.loop = sound.loop;
+            sound.source.outputAudioMixerGroup = sound.mixerGroup;
+        }
+    }
+
+    void Start()
+    {
+        PlaySound(sceneMusic);
+    }
+
+    public void PlaySound(string _name)
+    {
+        Sound sound = Array.Find(sounds, s => s.name == _name);
+        sound.PlaySound();
+    }
+
+    public void ChangeMusic(string _name)
+    {
+        foreach (Sound sound in sounds)
+        {
+            if (sound.mixerGroup.name == "Music") 
+            {
+                sound.StopSound();
+            }
+        }
+        PlaySound(_name);
+    }
+
+     public static AudioManager GetInstance() 
+     {
+        return instance;
+     }
+}
