28bf3bb9fefdfa20ef0820cdfb9fb55020b9b793
FakeBrett <brettloland@gmail.com>
2020-03-31 20:11:50 -0700

    Hover to Select
    Hovering over buttons in the Main Menu and Pause Menu will now switch them to being the currently selected button.
    -Added new Script called Hover Select
    -Added new event and this hover script to each button

---
+++ Assets/Scripts/PlayerScripts/HoverSelect.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HoverSelect : MonoBehaviour
+{
+    public Button card;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void SelectButton()
+    {
+        card.Select(); //sets 'resume button' to selected when you open the pause menu
+        card.OnSelect(null);
+    }
+}
