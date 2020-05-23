24326ddb8bfbad8cc11528a61cd600d5f6c9a0ef
FakeBrett <brettloland@gmail.com>
2020-05-23 12:57:56 -0700

    More Colliders and Comment
    -Attached neutral colliders to all the buildings
    -Added a comment to the Building.cs Update function that offers a different solution if we don't want to include the Update function.

--- Assets/Scripts/Building.cs
+++ Assets/Scripts/Building.cs
@@ -29,6 +29,7 @@ public class Building : RangedUnit
         transform.position = new Vector3(transform.position.x, transform.position.y, 1f);
     }
 
+    //instead of calling this update, I could attach the buildings and neutral colliders to an object, and if the building is null then active the collider
     private void Update() //check to see if building destroyed to active neutral summoning area
     {
         if (this.health <= 0)
